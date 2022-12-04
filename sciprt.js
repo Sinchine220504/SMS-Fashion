69bfcd5a0c5d4b425a0d6f3ad685480767b30c9a
Saks34 <119040050+Saks34@users.noreply.github.com>
2022-12-04 22:40:57 +0530

    Create sciprt.js

---
+++ sciprt.js
@@ -0,0 +1,19 @@
+let searchbar = document.querySelector('#search-bar')
+let searchbox = document.querySelector('#search-box')
+let shop = document.querySelector('#shop-cart')
+let shopcart = document.querySelector('#.shopping-cart')
+let  menubar= document.querySelector('#menu-bar')
+let  mynav= document.querySelector('#navbar')
+
+
+
+
+searchbar.onclick = () = {
+  searchbox.classlist.toggle('active'
+}
+shop.oneclick = () ={
+   shopcart.classlist.toggle('active');
+}
+menubar.oneclick{
+ mynav.classlist.toggle('active');
+ }
